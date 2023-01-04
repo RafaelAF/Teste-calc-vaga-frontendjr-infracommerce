9f27d40e2a2e011ac7e56411d75212cd6cb6fe99
RafaelAF <andrade.rafa71@gmail.com>
2023-01-04 17:08:56 -0300

    ajustando funções

--- script.js
+++ script.js
@@ -1,118 +1,31 @@
 const display = document.getElementById('output')
 const btns = document.querySelectorAll('.btn div')
+const memory = document.getElementById('memory')
 
 let valorInput1 = '';
 let valorInput2 = '';
+
+let operator = '';
 
 let x = 0;
 let y = 0;
 
 let resultado = 0;
-let contarVoltas = 0;
+let contarVoltas = 1;
 let contarClick = 0;
 
-let isOperating = false;
-let countDot = 0;
 
 const somar = (x, y) =>{
-    if(contarVoltas == 0){
-        let x1 = x;
-        y = 0;
-        valorInput2 = '';
-        resultado = x1 + y;
-        console.log(resultado)
-    }else if(contarVoltas == 1){
-        contarVoltas = 2
-        let x1 = x;
-        x = 0;
-        valorInput2 = '';
-        resultado = x1 + y;
-        display.innerHTML = resultado;
-        console.log(resultado)
-        
-    }else if(contarVoltas == 2){
-        let x1 = resultado;
-        valorInput2 = '';
-        resultado = x1 + y;
-        display.innerHTML = resultado;
-        console.log(resultado)
-    }
     return x + y
 }
 
 const subtrair = (x, y) =>{
-    if(contarVoltas == 0){
-        let x1 = x;
-        y = 0;
-        valorInput2 = '';
-        resultado = x1 - y;
-        console.log(resultado)
-    }else if(contarVoltas == 1){
-        contarVoltas = 2
-        let x1 = x;
-        x = 0;
-        valorInput2 = '';
-        resultado = x1 - y;
-        display.innerHTML = resultado;
-        console.log(resultado)
-        
-    }else if(contarVoltas == 2){
-        let x1 = resultado;
-        valorInput2 = '';
-        resultado = x1 - y;
-        display.innerHTML = resultado;
-        console.log(resultado)
-    }
     return x - y
 }
 const multiplicar = (x, y) =>{
-    if(contarVoltas == 0){
-        let x1 = x;
-        y = 0;
-        valorInput2 = '';
-        resultado = x1 * y;
-        console.log(resultado)
-    }else if(contarVoltas == 1){
-        contarVoltas = 2
-        let x1 = x;
-        x = 0;
-        valorInput2 = '';
-        resultado = x1 * y;
-        display.innerHTML = resultado;
-        console.log(resultado)
-        
-    }else if(contarVoltas == 2){
-        let x1 = resultado;
-        valorInput2 = '';
-        resultado = x1 * y;
-        display.innerHTML = resultado;
-        console.log(resultado)
-    }
     return x * y
 }
 const dividir = (x, y) =>{
-    if(contarVoltas == 0){
-        let x1 = x;
-        y = 0;
-        valorInput2 = '';
-        resultado = x1 / y;
-        console.log(resultado)
-    }else if(contarVoltas == 1){
-        contarVoltas = 2
-        let x1 = x;
-        x = 0;
-        valorInput2 = '';
-        resultado = x1 / y;
-        display.innerHTML = resultado;
-        console.log(resultado)
-        
-    }else if(contarVoltas == 2){
-        let x1 = resultado;
-        valorInput2 = '';
-        resultado = x1 / y;
-        display.innerHTML = resultado;
-        console.log(resultado)
-    }
     return x / y
 }
 
@@ -120,11 +33,14 @@ const dividir = (x, y) =>{
 const resetar = () =>{
     x = 0;
     y = 0;
-
+    operator = '';
     valorInput1 = '';
     valorInput2 = '';
-    display.innerHTML = '0';
-    contarVoltas = 0;
+    display.value = 0;
+    memory.innerHTML = '';
+    contarVoltas = 1;
+    contarClick = 0;
+    resultado = 0;
 }
 
 
@@ -135,77 +51,177 @@ btns.forEach((e)=>{
     e.addEventListener('click', ()=>{
         switch (e.innerHTML) {
             case '+':
-                if(contarVoltas == 0){
-                    contarVoltas = 1
-                    countDot = 0
-                }else{
-                    somar(x, y)
-                    isOperating = true
-                    countDot = 0
+                contarClick += 1;
+                if(contarClick == 1){
+                    operator = '+'
+                    if(x){
+                        memory.innerHTML = `${x} ${operator}`
+                        display.value = 0;
+                        contarVoltas = 2;
+                        
+                    }
                 }
+                if(contarClick > 1){
+                    if(contarVoltas == 2){
+                        resultado = somar(x, y)
+                        display.value = resultado;
+                        memory.innerHTML = ''
+                        x = resultado
+                    }
+                    
+                }
+                if(contarClick == 3){
+
+                }
+                
                 break;
             case '-':
-                if(contarVoltas == 0){
-                    contarVoltas = 1
-                }else{
-                    subtrair(x, y)
-                    isOperating = true
+                contarClick += 1;
+                if(contarClick == 1){
+                    operator = '-'
+                    if(x){
+                        memory.innerHTML = `${x} ${operator}`
+                        display.value = 0;
+                        contarVoltas = 2;
+                        
+                    }
+                }
+                if(contarClick > 1){
+                    if(contarVoltas == 2){
+                        resultado = subtrair(x, y);
+                        display.value = resultado;
+                        memory.innerHTML = ''
+                        x = resultado
+                    }
+                    
+                }
+                if(contarClick == 3){
+
                 }
                 
                 break;
             case '*':
-                if(contarVoltas == 0){
-                    contarVoltas = 1
-                }else{
-                    multiplicar(x, y)
-                    isOperating = true
+                contarClick += 1;
+                if(contarClick == 1){
+                    operator = '*'
+                    if(x){
+                        memory.innerHTML = `${x} ${operator}`
+                        display.value = 0;
+                        contarVoltas = 2;
+                        
+                    }
+                }
+                if(contarClick > 1){
+                    if(contarVoltas == 2){
+                        resultado = multiplicar(x, y)
+                        display.value = resultado;
+                        memory.innerHTML = ''
+                        x = resultado
+                    }
+                    
+                }
+                if(contarClick == 3){
+
                 }
                 
                 break;
             case '/':
-                if(contarVoltas == 0){
-                    contarVoltas = 1
-                }else{
-                    dividir(x, y)
-                    isOperating = true
+                contarClick += 1;
+                if(contarClick == 1){
+                    operator = '/'
+                    if(x){
+                        memory.innerHTML = `${x} ${operator}`
+                        display.value = 0;
+                        contarVoltas = 2;
+                        
+                    }
+                }
+                if(contarClick > 1){
+                    if(contarVoltas == 2){
+                        resultado = dividir(x, y)
+                        display.value = resultado;
+                        memory.innerHTML = ''
+                        x = resultado
+                    }
+                    
+                }
+                if(contarClick == 3){
+
                 }
                 
                 break;
             case '=':
-                x = 0;
-                y = 0;
-                if(resultado){
-                    if(isOperating){
-                        console.log(resultado)
-                    }
+                if(operator == '+' && contarVoltas == 2){
+                    display.value = somar(x, y);
+                    memory.innerHTML = '';
+                    x = 0;
+                    contarVoltas = 3;
+                    contarClick = 0;
+                    //console.log(contarVoltas)
+                }else if(operator == '-' && contarVoltas == 2){
+                    display.value = subtrair(x, y);
+                    memory.innerHTML = '';
+                    contarVoltas = 3;
+                    contarClick = 0;
+                }else if(operator == '*' && contarVoltas == 2){
+                    display.value = multiplicar(x, y);
+                    memory.innerHTML = '';
+                    contarVoltas = 3;
+                    contarClick = 0;
+                }else if(operator == '/' && contarVoltas == 2){
+                    display.value = dividir(x, y);
+                    memory.innerHTML = '';
+                    contarVoltas = 3;
+                    contarClick = 0;
                 }
+                
                 break;
             case 'reset':
                 resetar()
                 break;
             case 'del':
-                // usar o slice para remover o ultimo item
-                console.log('deletar ultimo caractere digitado')
-                break;
-            case '.':
-                if(countDot == 0){
-                    valorInput1.innerHTML += e.innerHTML; 
-                    //countDot = 1
+                if(display.value == '' || display.value != 0){
+                    if(contarVoltas == 1){
+
+                        valorInput1 = parseFloat(display.value.slice(0, -1));
+                        x = valorInput1
+
+                        display.value = parseFloat(x);
+                    }else if(contarVoltas == 2){
+                        valorInput2 = parseFloat(display.value.slice(0, -1));
+                        y = parseFloat(display.value.slice(0, -1));
+                        display.value = parseFloat(y);
+                    }
                 }
                 break;
+            case ',':
+                    //countDot = 1
+                    if(contarVoltas == 1){
+                        x += e.innerHTML
+                        display.value = x;
+                    }else if(contarVoltas == 2){
+                        valorInput2 += e.innerHTML;
+                        y = parseFloat(valorInput2)
+                        display.value = y;
+                    }
+                break;
             default:
-                if(contarVoltas == 0){
+                if(contarVoltas == 1){
                     valorInput1 += e.innerHTML;
-                    x = parseInt(valorInput1)
-                    display.innerHTML = valorInput1;
-                }else if(contarVoltas == 1){
+                    x = parseFloat(valorInput1)
+                    display.value = x;
+                }else if(contarVoltas == 2){
                     valorInput2 += e.innerHTML;
-                    y = parseInt(valorInput2)
-                    display.innerHTML = valorInput2;
-                }else{
-                    valorInput2 += e.innerHTML;
-                    y = parseInt(valorInput2)
-                    display.innerHTML = valorInput2;
+                    y = parseFloat(valorInput2)
+                    display.value = y;
+                }else if(contarVoltas == 3){
+                    valorInput1 = e.innerHTML;
+                    display.value = parseFloat(valorInput1);
+                    
+                    valorInput2 = '';
+                    x = 0;
+                    y = 0;
+                    contarVoltas = 1;
                 }
                 //console.log(x ,y);
                 break;
